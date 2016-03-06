4db9e0cfdc57168420d476cd71ca755fb209c819
PKuhn <patrick.kuhn@student.hpi.de>
2016-03-06 17:34:35 +0100

    Added class to read in glove word embeddings

--- dataset/semeval.py
+++ dataset/semeval.py
@@ -1,4 +1,22 @@
+import numpy as np
 from dataset.dataset import Dataset
+
+
+class Embeddings:
+    def __init__(self, embedding_path):
+        self.embeddings = self._load_embeddings(embedding_path)
+
+    def __getitem__(self, word):
+        return np.array(self.embeddings[word])
+
+    def _load_embeddings(self, embedding_path):
+        embeddings = {}
+
+        with open(embedding_path) as embedding_file:
+            for line in embedding_file:
+                splitted = line.split()
+                embeddings[splitted[0]] = splitted[1:]
+        return embeddings
 
 
 class SemEval(Dataset):
